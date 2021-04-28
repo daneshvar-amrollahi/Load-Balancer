edbae8345af7312b837596a646d23425b72f5785
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-04-29 00:05:52 +0430

    separating values by comma done

--- main.cpp
+++ main.cpp
@@ -26,24 +26,62 @@ void get_file_paths(const int &argc, char* argv[], string &traits_path, string &
     users_path = string(argv[2]);
 }
 
-vector<string> findClosest(const string& traits_path, const string& users_path)
+vector<string> getLines(const string& traits_path, const string& users_path)
 {
     vector<string> ans;
-
     ifstream fin;
     fin.open(traits_path);
 
     string line;
     
-    fin >> line;
-    ans.push_back(line);
-    
-    fin >> line;
-    ans.push_back(line);
+    while (getline(fin, line))
+        ans.push_back(line);
 
     fin.close();
+    return ans;
+}
+
+vector<int> separateByComma(string line)
+{
+    vector<int> ans;
+
+    string cur = "";
+    for (auto c: line)
+    {
+        if (c == ',')
+        {
+            ans.push_back(stoi(cur));
+            cur = "";
+        }
+        else
+            cur += c;
+    }
+    ans.push_back(stoi(cur));
 
     return ans;
+}
+
+vector<vector<int> > getTraits(vector<string> lines)
+{
+    vector<vector<int> > ans;
+    for (auto line: lines)
+        ans.push_back(separateByComma(line));
+    return ans;
+}
+
+vector<string> findClosest(const string& traits_path, const string& users_path)
+{
+    vector<string> lines = getLines(traits_path, users_path);
+    vector<vector<int> > traits = getTraits(lines);
+
+    for (int i = 0 ; i < traits.size() ; i++)
+    {
+        for (int j = 0 ; j < traits[i].size() ; j++)
+            cout << traits[i][j] << " ";
+        cout << endl;
+    }
+
+    return lines;
 }
 
 int main(int argc, char *argv[])
@@ -55,7 +93,6 @@ int main(int argc, char *argv[])
 
     vector<string> ans = findClosest(traits_path, users_path);
 
-    cout << ans[0] << "\n" << ans[1] << endl;
 
     
     return 0;
